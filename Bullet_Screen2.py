450c5267af1053c044836bb5e8a4902a81e34d5e
huzeyusir <52703613+huzeyusir@users.noreply.github.com>
2020-10-23 01:38:03 +0800

    Add files via upload

---
+++ Bullet_Screen2.py
@@ -0,0 +1,149 @@
+#  -*- coding: utf-8 -*-
+"""
+Created on Thu May 21 12:01:25 2020
+@author: huzey
+
+读取Rank List2 产生的csv文件，并下载为txt文件，里面包含time comment两个类别
+csv文件和产生的txt文件在同一文件夹下
+程序会自动为不同分区建立子文件夹
+使用方法：在标记处直接修改保存地址，运行后就可得到一堆csv文件
+！！！请勿随意改动
+针对2020.10.16改版之后的新B站排行榜
+https://www.bilibili.com/v/popular/rank/all
+"""
+
+import pandas as pd
+import requests
+import re
+from bs4 import BeautifulSoup
+import operator
+import numpy as np
+   
+
+# 获取url
+def getHTMLText(url):
+    try:
+        # print("获取url当中")
+        re=requests.get(url,timeout=5000)
+        re.raise_for_status()
+        re.encoding=re.apparent_encoding
+        print("获取url完成")
+        return re.text
+    except:
+        print("获取Url失败")
+
+def parsePage(text):
+    try:
+        # print("解析文本...")
+        keyStr = re.findall(r'"cid":[\d]*',text)
+        # B站有两种寻址方式，一种是API，一种是正则表达式搜索，第二种多一些
+        if not keyStr:
+            # 若列表为空，则等于“False”
+            keyStr = re.findall(r'cid=[\d]*', text)
+            key = eval(keyStr[0].split('=')[1])
+        else:
+            key = eval(keyStr[0].split(':')[1])
+        commentUrl = 'https://comment.bilibili.com/' + str(key) + '.xml'  
+        #  弹幕存储地址
+        # print("获取弹幕")
+        commentText=getHTMLText(commentUrl)
+        soup = BeautifulSoup(commentText, "html.parser")
+        # soup2=BeautifulSoup(text,"html.parser")
+        commentList={}
+        
+        # find()方法，获取文本，去掉空格
+        for comment in soup.find_all('d'):
+            time=float(comment.attrs['p'].split(',')[0])
+            # tag.attrs（标签属性，字典类型）
+            commentList[time]=comment.string
+        newDict=sorted(commentList.items(),key=operator.itemgetter(0))
+        # 字典排序
+        commentList=dict(newDict)
+        print("解析文本完成")
+        return commentList,key
+    except:
+        print("解析失败")
+        
+        
+ 
+def float2time(f):
+    timePlus=int(f)
+    m=timePlus//60
+    s=timePlus-m*60
+    return str(m)+':'+str(s).zfill(2)
+ 
+def ioFunc(commentList,root):
+    print("写入文本中...")
+    path = root + '.txt'
+    print(path)
+    f = open(path, 'w',encoding='utf-8')
+    ws = "{:7}\t{}\n".format('time', 'comment')
+    f.write(ws)
+    lastTime=0
+    for time,string in commentList.items():# 记得items()
+        lastTime = float2time(time)
+        ws = "{:7}\t{}\n".format(lastTime,string)
+        f.write(ws) 
+        #  手动换行
+
+
+
+# 并非要爬取所有内容
+#已经没有月周日排行榜了
+typeItem =  ['动画','音乐','舞蹈','游戏','科技','生活','鬼畜','时尚']
+import time 
+import re     
+import os
+"""在此处修改地址"""
+filepath = 'D:/bilibili/2020_10_21/'
+
+"""每次爬取记得修改分区（因为一次爬取太多会被B站屏蔽IP，烦死了）"""
+for typetemp in typeItem :#  在这里修改分区   typeItem4   
+    filename = filepath + str(typetemp) + '.csv'
+    print(filename)
+    with open(filename,encoding='utf_8_sig') as file:
+        BVInfo = pd.read_csv(file)
+        BVUrl = BVInfo['url']
+        Title = BVInfo['tittle']
+        AllTittle=[]
+        for OneUrl in BVUrl:
+            #  .values 去掉可恶的name，dtype
+            OneTittle = BVInfo[BVInfo.url== OneUrl]['tittle'].values 
+            # print("标题：",OneTittle,type(OneTittle))
+            print('网址：',OneUrl,type(OneUrl))
+            temp_url = re.search(r"(?<=https://www.bilibili.com/video/)\S+",OneUrl).group() # 使用group(num) 或 groups() 匹配对象函数来获取匹配内容。
+            # print(temp_url)
+            StrOneTittle = str(OneTittle)
+            
+            text=getHTMLText(OneUrl)
+            
+            try:
+                commentList,key=parsePage(text)
+            except:
+                print("解析跳过")
+                # 跳过解析失败的
+                continue    
+            # 判断是否有文件夹存在，如果没有就创建
+            DocPath  = filepath+str(typetemp)+ '/'
+            
+            if not os.path.exists(DocPath):
+                os.makedirs(DocPath) 
+                print(DocPath)
+                
+                
+            SavePath =  DocPath + str(key)
+            if not os.path.exists(  SavePath + '_'+temp_url ):
+                # 弹幕编号 + BV号
+                ioFunc(commentList,SavePath +'_'+temp_url)
+                print("Finish.")
+
+    time.sleep(180) #表示秒  爬取一个分区休息3min
+    print("----Pause---3min----")
+
+
+
+
+
+
+
+
---
+++ Rank List1.py
@@ -0,0 +1,147 @@
+# -*- coding: utf-8 -*-
+
+"""
+Created on Wed May 20 16:49:37 2020
+BILIBILI 视频列表信息爬取
+@author: huzey
+爬取思路
+1.分别在不同排行榜下爬取信息
+2.信息包含有：视频BV号,BV地址，视频排名，视频名称，综合评分，up主名称
+3.将这些信息保存在一个DataFrame里，最后保存为Excel表格
+使用方法：在标记处直接修改保存地址，运行后就可得到一堆csv文件
+！！！请勿随意改动
+针对2020.10.16以前，改版的旧的B站排行榜网页
+"""
+import requests
+import re
+from bs4 import BeautifulSoup
+import traceback
+
+class Spider:#常用的爬取方法的简单封装
+    def __init__(self,url):
+        self.url=url
+        
+    def getHTML(self):#获取html的对应代码
+        headers={'User-Agent':'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.103 Safari/537.36'}
+       
+        try:
+            response=requests.get(url=self.url,headers=headers,timeout=20)
+            response.raise_for_status()
+            response.encoding="utf_8_sig"
+            return response.text
+        except:
+            return "网页访问失败"
+        
+    def setSoup(self):#获取soup对象
+        html=self.getHTML()
+        self.soup=BeautifulSoup(html,'html.parser')
+    
+    def findTag(self,tagName):#按照标签名查找标签
+        return self.soup.find_all(tagName)
+    
+    def findTagByAttrs(self,tagName,attrs):
+        return self.soup.find_all(tagName,attrs)
+    
+    def getBeautifyHTML(self):
+        return self.soup.prettify()
+
+# 大人，时代变了！
+def getURLFromBilibili():# 获取各种各样排行的榜单的信息
+    date={
+        #1:'日排行',
+        3:'三日排行',
+        7:'周排行',
+        30:'月排行'
+    }
+    #总共收集8个分区的弹幕
+    areatype={
+        #0:'全站',
+        1:'动画',  
+        #168:'国漫相关',
+        3:'音乐',
+        129:'舞蹈',
+        4:'游戏',
+        36:'科技',
+        #188:'数码',
+        160:'生活',
+        119:'鬼畜',
+        155:'时尚',
+        #5:'娱乐',
+        #181:'影视'
+    }
+    ranktype={
+       'all':'全站',
+       #'origin':'原创'
+    }
+    submit={
+        '0':'全部投稿',
+        #'1':'近期投稿'
+    }  
+    urlDict={}#存放相应url的字典
+    for ranktypeItem in ranktype.keys():
+        for areatypeItem in areatype.keys():
+            for submitItem in submit.keys():
+                for dateTypeItem in date.keys():
+                    title=ranktype[ranktypeItem]+'_'+areatype[areatypeItem]+'_'+submit[submitItem]+'_'+date[dateTypeItem]
+                    destinaTionUrl='https://www.bilibili.com/ranking/{}/{}/{}/{}'.format(ranktypeItem,areatypeItem,submitItem,dateTypeItem)
+                    urlDict[title]=destinaTionUrl
+                    
+    return urlDict
+
+'''
+学习一下 find_all()
+'''
+def getPage(url):
+    # 爬取单个页面,核心代码，返回页面的list
+    spider=Spider(url)
+    spider.setSoup()
+    itemList=spider.findTagByAttrs('li','rank-item')
+    pageContentList=[]
+    for item in itemList:
+       
+        pageContentItem=[]
+        for title in item.find_all('a','title'):
+            pageContentItem.append(title.string)
+            #print(title.string)
+               
+        for bvurl in item.find_all('a',class_="title"):
+            BVurl=bvurl.get("href")
+            pageContentItem.append(BVurl)
+            #print(BVurl)   
+          
+        for playnum in item.find_all('span','data-box'):
+            pattern=r">([^<]+)<"
+            n=re.findall(pattern,playnum.__str__())[0]
+            pageContentItem.append(n)
+            #print(n)
+
+        
+        pageContentItem.append(item.find_all('div','pts')[0].div.string)
+        pageContentList.append(pageContentItem)
+        
+    return pageContentList   
+
+
+urlDict = getURLFromBilibili()
+import pandas as pd 
+import os
+
+"""修改地址"""
+out_path="D:/bilibili/2020_10_21/"
+
+if not os.path.exists(out_path):
+    os.makedirs(out_path)
+    print("Path:",out_path)
+
+for urlName in urlDict:
+    print("正在处理"+urlName+"页面...")
+    url=urlDict[urlName]
+    #在字典里查找
+    pageList=getPage(url)
+    #print(pageList) 
+    
+    PageName = str(urlName)    
+    BvDataFrame = pd.DataFrame(pageList,columns=['tittle','url',"view",'barrage','uploader','score'])          
+    BvDataFrame.to_csv(out_path+PageName+'.csv',index=False,encoding='utf_8_sig')
+
+
